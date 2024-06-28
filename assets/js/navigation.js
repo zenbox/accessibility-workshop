5f89240bff86e0b7e6dd5f936c137512d53e5372
zenbox <michael@zenbox.de>
2024-06-28 07:16:39 +0200

    update keyboard control

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,24 +1,23 @@
 // Du bist JS-Entwickler. Schreibe eine Funktion, die aus allen Einträgen in einem JSON-Objekt eine Navigation erstellt. Das Objekt enthäkt den Dateiname und einen Linkttext pro Eintrag. Die Navigation soll als `ol`-Element in einer `nav`-Gruppe im `footer`der Seite integriert werden. Das Skript muss als Client-Skript im Browser laufen.
 
-// JSON-Objekt
-const navigation = [
-    { file: "index.html", text: "Semantik" },
-    { file: "color-and-contrast.html", text: "Farbe und Kontrast" },
-    { file: "keyboard-controls.html", text: "Tastatur-Steuerung" },
-    { file: "typography.html", text: "Schrift und Satz" },
-    { file: "form.html", text: "Formular" },
-    { file: "navigation.html", text: "Navigation" },
-    { file: "non-text-content.html", text: "Bild und Grafik" },
-    { file: "parallax.html", text: "Bewegung" },
-    { file: "pattern.html", text: "Pattern" },
-]
-
 // Funktion
 function createNavigation() {
     const nav = document.createElement("nav")
     const ol = document.createElement("ol")
 
-    nav.id = "links"
+    const navigation = [
+        { file: "index.html", text: "Semantik" },
+        { file: "color-and-contrast.html", text: "Farbe und Kontrast" },
+        { file: "keyboard-controls.html", text: "Tastatur-Steuerung" },
+        { file: "typography.html", text: "Schrift und Satz" },
+        { file: "form.html", text: "Formular" },
+        { file: "navigation.html", text: "Navigation" },
+        { file: "non-text-content.html", text: "Bild und Grafik" },
+        { file: "parallax.html", text: "Bewegung" },
+        { file: "pattern.html", text: "Pattern" },
+    ]
+
+    nav.id = "main-navigation"
 
     navigation.forEach((entry) => {
         const li = document.createElement("li")
@@ -33,23 +32,44 @@ function createNavigation() {
 
     nav.appendChild(ol)
     document.body.prepend(nav)
+}
 
+function createSkipLinks() {
+    // - - - - - - - - - -
+    // Build a skip link navigation
+    // - - - - - - - - - -
     const skipLinks = document.createElement("nav")
     skipLinks.id = "skip-links"
     const links = [
-        { href: "#main-content", text: "zum Inhalt" },
-        { href: "#main-navigation", text: "zur Hauptnavigation" },
-        { href: "#main-search", text: "zur Suche" },
-        { href: "#header", text: "zur Kopf-Sektion" },
-        { href: "#footer", text: "zur Fuß-Sektion" },
+        { selector: "body > main", href: "#main-content", text: "zum Inhalt" },
+        {
+            selector: "body > nav",
+            href: "#main-navigation",
+            text: "zur Hauptnavigation",
+        },
+        {
+            selector: "body [type=search]",
+            href: "#main-search",
+            text: "zur Suche",
+        },
+        {
+            selector: "body > header",
+            href: "#header",
+            text: "zur Kopf-Sektion",
+        },
+        { selector: "body > footer", href: "#footer", text: "zur Fuß-Sektion" },
     ]
 
     links.forEach((link) => {
-        const a = document.createElement("a")
-        a.href = link.href
-        a.textContent = link.text
+        let el = document.querySelector(link.selector)
+        if (el) el.id = link.href.substr(1, link.href.length)
+        if (document.querySelector(`${link.href}`)) {
+            const a = document.createElement("a")
+            a.href = link.href
+            a.textContent = link.text
 
-        skipLinks.appendChild(a)
+            skipLinks.appendChild(a)
+        }
     })
 
     skipLinks.addEventListener("keydown", (event) => {
@@ -62,5 +82,45 @@ function createNavigation() {
     document.body.prepend(skipLinks)
 }
 
+function createDeveloperButton() {
+    const developerButton = document.createElement("button")
+    let developerButtonState
+
+    if (localStorage.getItem("developer") === "on") {
+        developerButtonState = true
+        document.body.classList.add("developer")
+        developerButton.classList.add("on")
+    } else {
+        developerButtonState = false
+        developerButton.classList.add("off")
+    }
+
+    developerButton.tabIndex = -1
+
+    developerButton.addEventListener("click", () => {
+        developerButtonState = !developerButtonState
+        if (developerButtonState) {
+            document.body.classList.add("developer")
+            developerButton.classList.remove("off")
+            developerButton.classList.add("on")
+            localStorage.setItem("developer", "on")
+        } else {
+            document.body.classList.remove("developer")
+            developerButton.classList.remove("on")
+            developerButton.classList.add("off")
+            localStorage.setItem("developer", "off")
+        }
+    })
+    document.querySelector("#main-navigation ol").prepend(document.createElement("li"))
+    document.querySelector("#main-navigation ol li:first-child").prepend(developerButton)
+}
+// - - - - - - - - - -
 // Aufruf
-document.addEventListener("DOMContentLoaded", createNavigation)
+// - - - - - - - - - -
+document.addEventListener("DOMContentLoaded", () => {
+    createNavigation()
+    createSkipLinks()
+    createDeveloperButton()
+
+    document.querySelector("html").setAttribute("lang", "de")
+})
